e08963179b58de19927e86a1ae3f151a186b5297
FL0W_4RT- <hawtstrokes1337@gmail.com>
2022-10-22 13:44:07 +0800

    Revert "Add Minimize Support"
    This reverts commit 169c92a0520617891f716edf48692485014757c8.

--- MakeStringMicrosoftCompliant/Source/MakeStringMC.cpp
+++ MakeStringMicrosoftCompliant/Source/MakeStringMC.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <filesystem>
 
-static void CreateMicrosoftCompliantStringCode(const std::string& inputFileName, const std::string& outputFileName, bool isMinimized = false, unsigned int maxSize = 2048) {
+inline void CreateMicrosoftCompliantStringCode(const std::string& inputFileName, const std::string& outputFileName, unsigned int maxSize = 2048) {
 	std::vector<std::string> vecStr;
 	std::ifstream ifs(inputFileName);
 	std::string line;
@@ -18,8 +18,6 @@ static void CreateMicrosoftCompliantStringCode(const std::string& inputFileName,
 				buffer = "";
 				count = 0;
 			}
-			if (isMinimized && c == ' ') continue;	// skip white spaces if minimized
-			if (isMinimized && c == '\t') continue;	// skip tabs if minimized
 			buffer += c;
 			++count;
 		}
@@ -37,32 +35,18 @@ static void CreateMicrosoftCompliantStringCode(const std::string& inputFileName,
 
 int main(int argc, char** argv) {
 	auto Help = []() {
-		std::cout << "./MakeStringMC <inputFile> <outputFile> <options>" << std::endl;
-		std::cout << "Options:\nm\tMinimize Output" << std::endl;
+		std::cout << "./MakeStringMC <inputFile> <outputFile>" << std::endl;
 	};
 
-	if (argc > 4 || argc < 3) {
+	if (argc != 3) {
 		Help();
 		return -1;
 	}
 
-	if (!std::filesystem::exists(argv[1])) {std::cout << "isMinimized = true" << std::endl;
+	if (!std::filesystem::exists(argv[1])) {
 		std::cout << "Input File Does Not Exist!!!" << std::endl;
 		return -1;
 	}
 
-
-	bool isMinimized = false;
-	if (argc == 4)
-	{
-		if (std::string(argv[3]).find('m') != std::string::npos)
-		{
-			isMinimized = true;
-			std::cout << "isMinimized = true" << std::endl;
-		}
-		else std::cout << "isMinimized = false" << std::endl;
-	}
-	CreateMicrosoftCompliantStringCode(argv[1], argv[2], isMinimized);
-	std::cout << "Done!" << std::endl;
-	return 0;
+	CreateMicrosoftCompliantStringCode(argv[1], argv[2]);
 }
